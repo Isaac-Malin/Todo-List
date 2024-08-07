97d12f50345bec11f0d693f16c581604bb6c31b9
Isaac Malin <Isaacmalin3530@gmail.com>
2024-08-07 11:41:05 -0600

    TodoInput is hooked up with state and correctly setting the input value

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import TodoList from './Components/TodoList';
+import TodoCard from './Components/TodoCard';
+import TodoInput from './Components/TodoInput';
 
 function App() {
+
+  
   return (
     <div className="App">
-      
+      <TodoInput />
+      <TodoList />
+      <TodoCard />
     </div>
   );
 }
--- src/Components/TodoInput.jsx
+++ src/Components/TodoInput.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function TodoInput() {
+
+  const [todoInput, setTodoInput] = useState('')
+
+  const handleTodoInput = (e) => {
+    setTodoInput(e.target.value)
+  }
+
   return (
-    <div>TodoInput</div>
+    <div className='input-container'>
+      <input
+        className='input-text'
+        type="text"
+        value={todoInput}
+        onChange={(e) => handleTodoInput(e)}
+      />
+    </div>
   )
 }
